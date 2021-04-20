f8bf5c088be1ca1232f46280a84ac6cae229c18c
huangrkul <huangrkul@gmail.com>
2021-04-19 22:39:17 -0700

    added new demo and fixed some styling issues on homepage and demo page

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,38 +1,63 @@
-import React, {useEffect} from 'react';
-import { setAni } from '../js/snippets';
+import React, { useEffect } from "react";
+import { setAni } from "../js/snippets";
+import poster3D from "../../public/assets/posters/3d_poster.jpg";
+import poster2019 from "../../public/assets/posters/2019_poster.jpg";
+import posterMP from "../../public/assets/posters/mp_poster.jpg";
 
 const Demo = (props) => {
-
   useEffect(() => {
-    setAni('h1',0,'enter-bottom');
-    setAni('.demos-content > section:first-child',100,'section-enter');
-    setAni('.demos-content > section:last-child',200,'section-enter');
-  },[])
+    setAni("h1", 0, "enter-bottom");
+    setAni(".demos-content > section:first-child", 100, "section-enter");
+    setAni(".demos-content > section:nth-child(2)", 100, "section-enter");
+    setAni(".demos-content > section:last-child", 200, "section-enter");
+  }, []);
 
   return (
     <article className="demos-page">
       <h1 className="hide">DEMO REELS</h1>
       <div className="demos-content">
         <section className="hide">
-          <video className="demo-2d-vid" width="100%" height="100%" controls>
-            <source src="http://willhuanganimator.com/media/wh_demo.mp4" type="video/mp4"/>
-            <source src="http://willhuanganimator.com/media/wh_demo.ogg" type="video/ogg"/>
+          <video width="100%" height="100%" controls poster={posterMP}>
+            <source
+              src="http://willhuanganimator.com/media/wh_mediapro.mp4"
+              type="video/mp4"
+            />
+            <source
+              src="http://willhuanganimator.com/media/wh_mediapro.ogg"
+              type="video/ogg"
+            />
             Your browser does not support the video tag.
           </video>
         </section>
         <section className="hide">
-          <video className="demo-3d-vid" width="100%" height="100%" controls>
-            <source src="http://willhuanganimator.com/media/wh_3d_demo.mp4" type="video/mp4"/>
-            <source src="http://willhuanganimator.com/media/wh_3d_demo.ogg" type="video/ogg"/>
+          <video width="100%" height="100%" controls poster={poster2019}>
+            <source
+              src="http://willhuanganimator.com/media/wh_demo.mp4"
+              type="video/mp4"
+            />
+            <source
+              src="http://willhuanganimator.com/media/wh_demo.ogg"
+              type="video/ogg"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </section>
+        <section className="hide">
+          <video width="100%" height="100%" controls poster={poster3D}>
+            <source
+              src="http://willhuanganimator.com/media/wh_3d_demo.mp4"
+              type="video/mp4"
+            />
+            <source
+              src="http://willhuanganimator.com/media/wh_3d_demo.ogg"
+              type="video/ogg"
+            />
             Your browser does not support the video tag.
           </video>
         </section>
       </div>
     </article>
-  )
-
-}
+  );
+};
 
 export default Demo;
-
-
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react';
-import { setAni } from '../js/snippets';
-import logoWhite from '../../public/assets/homepage/logo-white.png';
-import logoCircle from '../../public/assets/homepage/logo-circle.png';
-import logo from '../../public/assets/homepage/logo.png';
-import titleHag from '../../public/assets/homepage/title-hag.png';
-import titleUn from '../../public/assets/homepage/title-un.png';
+import React, { useEffect } from "react";
+import { setAni } from "../js/snippets";
+import logoWhite from "../../public/assets/homepage/logo-white.png";
+import logoCircle from "../../public/assets/homepage/logo-circle.png";
+import logoNews from "../../public/assets/homepage/logo-news.png";
+import logo from "../../public/assets/homepage/logo.png";
+import titleHag from "../../public/assets/homepage/title-hag.png";
+import titleUn from "../../public/assets/homepage/title-un.png";
 
 let titleTimer;
 
@@ -12,50 +13,59 @@ const titleArray = [
   "Front-End Web Developer",
   "Multimedia Developer",
   "2D Animator",
-  "Banner Making Machine"
+  "Banner Making Machine",
 ];
 
 const Homepage = (props) => {
-
   const titleSequence = () => {
-    const titleDiv = document.querySelector('.title-seq');
+    const titleDiv = document.querySelector(".title-seq");
     let num = 0;
     titleDiv.innerText = titleArray[num];
-    titleDiv.classList.add('title-seq-intop');
+    titleDiv.classList.add("title-seq-intop");
     num++;
     titleTimer = setInterval(() => {
-      titleDiv.classList.remove('title-seq-intop');
-      titleDiv.classList.add('title-seq-outbtm');
+      titleDiv.classList.remove("title-seq-intop");
+      titleDiv.classList.add("title-seq-outbtm");
       setTimeout(() => {
         titleDiv.innerText = titleArray[num];
-        titleDiv.classList.remove('title-seq-outbtm');
-        titleDiv.classList.add('title-seq-intop');
+        titleDiv.classList.remove("title-seq-outbtm");
+        titleDiv.classList.add("title-seq-intop");
         num = num === titleArray.length - 1 ? 0 : num + 1;
       }, 300);
     }, 3000);
-  }
+  };
 
   useEffect(() => {
-    setAni('.logo-container img:first-child', 0, 'logo-enter');
-    setAni('.logo-container img:nth-child(2)', 300, 'logo-enter');
-    setAni('.logo-container img:last-child', 600, 'logo-enter-hero');
-    setAni('.title-container div:first-child', 1000, 'logo-animate');
-    setAni('.title-container div:nth-child(2) img:first-child', 1800, 'huang-inbtm');
-    setAni('.title-container div:nth-child(2) img:last-child', 1800, 'huang-intop');
-    setTimeout(() => {titleSequence()},2500);
-    setAni('.homepage ul', 2700, 'enter-bottom');
-    setAni('.homepage ul li:nth-child(1)', 3000, 'section-enter');
-    setAni('.homepage ul li:nth-child(2)', 3100, 'section-enter');
-    setAni('.homepage ul li:nth-child(3)', 3200, 'section-enter');
-    setAni('.homepage ul li:nth-child(4)', 3300, 'section-enter');
-    setAni('.homepage ul li:nth-child(5)', 3400, 'section-enter');
-    setAni('h6', 4000, 'enter-bottom');
+    setAni(".logo-container img:first-child", 0, "logo-enter");
+    setAni(".logo-container img:nth-child(2)", 300, "logo-enter");
+    setAni(".logo-container img:nth-child(3)", 600, "logo-enter-hero");
+    setAni(".title-container div:first-child", 1000, "logo-animate");
+    setAni(
+      ".title-container div:nth-child(2) img:first-child",
+      1800,
+      "huang-inbtm"
+    );
+    setAni(
+      ".title-container div:nth-child(2) img:last-child",
+      1800,
+      "huang-intop"
+    );
+    setTimeout(() => {
+      titleSequence();
+    }, 2500);
+    setAni(".homepage ul", 2700, "enter-bottom");
+    setAni(".homepage ul li:nth-child(1)", 3000, "section-enter");
+    setAni(".homepage ul li:nth-child(2)", 3100, "section-enter");
+    setAni(".homepage ul li:nth-child(3)", 3200, "section-enter");
+    setAni(".homepage ul li:nth-child(4)", 3300, "section-enter");
+    setAni(".homepage ul li:nth-child(5)", 3400, "section-enter");
+    setAni("h6", 4000, "enter-bottom");
+    setAni(".logo-container img:last-child", 4200, "logo-enter");
 
-    return(() => {
+    return () => {
       clearTimeout(titleTimer);
-    })
-  },[])
-
+    };
+  }, []);
 
   return (
     <article className="homepage">
@@ -63,32 +73,45 @@ const Homepage = (props) => {
         <div>
           <section className="logo-container logo-enter-hero">
             <img className="hide" src={logoWhite} />
-            <img className="hide" src={logoCircle}/>
-            <img className="hide" src={logo}/>
+            <img className="hide" src={logoCircle} />
+            <img className="hide" src={logo} />
+            <img className="hide" src={logoNews} />
           </section>
         </div>
         <div>
           <section className="title-container">
             <div></div>
             <div>
-              <img className="hide" src={titleHag}/>
-              <img className="hide" src={titleUn}/>
+              <img className="hide" src={titleHag} />
+              <img className="hide" src={titleUn} />
             </div>
             <div className="title-seq hide"></div>
           </section>
         </div>
       </section>
-      <ul className='hide'>
-        <li className='hide' onClick={() => props.changePage('btnAbout')}>Profile</li>
-        <li className='hide' onClick={() => props.changePage('btnProjects')}>Projects</li>
-        <li className='hide' onClick={() => props.changePage('btnBanners')}>Banners</li>
-        <li className='hide' onClick={() => props.changePage('btnDemo')}>Demos</li>
-        <li className='hide' onClick={() => props.changePage('btnContact')}>Contact</li>
+      <ul className="hide">
+        <li className="hide" onClick={() => props.changePage("btnAbout")}>
+          Profile
+        </li>
+        <li className="hide" onClick={() => props.changePage("btnProjects")}>
+          Projects
+        </li>
+        <li className="hide" onClick={() => props.changePage("btnBanners")}>
+          Banners
+        </li>
+        <li className="hide" onClick={() => props.changePage("btnDemo")}>
+          Demos
+        </li>
+        <li className="hide" onClick={() => props.changePage("btnContact")}>
+          Contact
+        </li>
       </ul>
-      <h6 className='hide'>(Last updated: 2/17/21) Copyright &copy; 2021 William Huang / huangrkul@gmail.com.  All Rights Reserved</h6>
+      <h6 className="hide">
+        (Last updated: 4/19/21) Copyright &copy; 2021 William Huang /
+        huangrkul@gmail.com. All Rights Reserved
+      </h6>
     </article>
-  )
-}
+  );
+};
 
 export default Homepage;
-
